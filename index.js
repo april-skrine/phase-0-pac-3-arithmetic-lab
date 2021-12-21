da3d0b3ed49b80096fb2cecc7e0a56e63a4fd6b0
April Skrine <april.skrine@gmail.com>
2021-12-21 15:21:19 -0600

    HOLY SHIT

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b) {
+    return a + b;
+}
+function subtract(a,b) {
+    return a - b;
+}
+function multiply(a,b) {
+    return a * b;
+}
+function divide(a,b) {
+    return a / b;
+}
+function increment(n) {
+    n++;
+    return n;
+}
+function decrement(n) {
+    n--;
+    return n;
+}
+function makeInt(number="2") {
+    return parseInt(number, 10);
+}
+function preserveDecimal(n="2.222") {
+    return parseFloat(n);
+}
